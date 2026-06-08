61ba7bbc3d9d7942d2c4614bd2f945242d965267
Delmu <delmu@example.com>
2026-06-08 08:17:41 +0700

    update

--- lib/constants.ts
+++ lib/constants.ts
@@ -53,7 +53,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
 
 export const PROFILE_STATS: ProfileStat[] = [
   { value: "Discord", label: "tof4z" },
-  { value: "Osu", label: "Delmu" },
+  { value: "Twitter", label: "@_Delmu" },
   { value: "Valorant", label: "ăn cháo đá cứt#1299" },
 ]
 
